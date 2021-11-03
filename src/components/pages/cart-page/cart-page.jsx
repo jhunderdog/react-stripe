fb559c32c0879a9fb578f4d7e1659caa1ba1129e
jhunderdog <33382water@gmail.com>
2021-11-03 22:30:16 +0900

    Cart Page Build 5

--- src/components/pages/cart-page/cart-page.jsx
+++ src/components/pages/cart-page/cart-page.jsx
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import { CartContext } from '../../../context/cart-context';
 import Layout from '../../shared/layout';
 import CartItem from './cart-item';
+import Total from './total';
 import './cart-page.styles.scss';
 
 const CartPage = () => {
@@ -20,6 +21,7 @@ const CartPage = () => {
                                 cartItems.map(item => <CartItem { ...item } key={item.id}/>)
                             }
                         </div>
+                        <Total itemCount={itemCount} total={total}/>
                     </div>
                     </>
                 }
