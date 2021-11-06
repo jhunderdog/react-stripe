a8d3773d315db0c63ff7f172320dc07d109e3089
jhunderdog <33382water@gmail.com>
2021-11-06 12:17:56 +0900

    create success & canceled pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import HomePage from './components/home-page';
 import Shop from './components/pages/shop/shop';
 import SingleProduct from './components/single-product/single-product';
 import CartPage from './components/pages/cart-page/cart-page';
+import Checkout from './components/checkout/checkout';
+import Success from './components/checkout/stripe-checkout/success';
+import Canceled from './components/checkout/stripe-checkout/canceled';
 import './App.scss';
 import NotFound from './components/not-found';
 
@@ -15,6 +18,9 @@ function App() {
         <Route path='/shop' component={Shop}/>
         <Route path='/product/:id' component={SingleProduct}/>
         <Route path='/cart' component={CartPage} />
+        <Route path='/checkout' component={Checkout} />
+        <Route path='/success' component={Success}/>
+        <Route path='/canceled' component={Canceled}/>
         <Route path='*' component={NotFound}/>
       </Switch>
     </div>
