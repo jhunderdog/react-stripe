32be1b179c02d49dc0a7b2ae10aa2f19c974ece4
jhunderdog <33382water@gmail.com>
2021-11-06 22:56:09 +0900

    Build Custom Checkout Component

--- src/components/checkout/checkout.jsx
+++ src/components/checkout/checkout.jsx
@@ -1,19 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CartContext } from '../../context/cart-context';
 import Layout from '../shared/layout';
 import StripeCheckout from './stripe-checkout/stripe-checkout';
+import ShippingAddress from './custom-checkout/shipping-address';
 import './checkout.styles.scss';
 
 const Checkout = () => {
     const { itemCount, total } = useContext(CartContext);
-
+    const [shipping, setShipping] = useState(null);
+    const addressShown = {
+        display: (shipping ? 'none' : 'block')
+    }
     return (
         <Layout>
             <div className='checkout'>
                 <h2>Checkout Summary</h2>
                 <h3>{`Total Items: ${itemCount}`}</h3>
                 <h4>{`Amount to Pay: $${total}`}</h4>
-                <StripeCheckout />
+                {/* <StripeCheckout /> */}
+                <div style={addressShown}>
+                    <ShippingAddress setShipping={setShipping}/>
+                </div>
             </div>
         </Layout>
     );
